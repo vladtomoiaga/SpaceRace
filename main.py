04341e5020c9844c3f3bdc7bf2c998b815f2cfa4
Vlad Tomoiaga <vlad.tomoiaga@msg.group>
2025-11-11 14:49:19 +0200

    Numpy added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This dataset was scraped from nextspaceflight.com and includes all the space missions since the beginning of Space Race between the USA and the Soviet Union in 1957!
-
+import numpy as np
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
